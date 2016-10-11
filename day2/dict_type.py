dd6f8a48f87d9361643f7cbac15eded27e4d65ae
pengbo14 <pengbo@jd.com>
2016-10-11 23:04:50 +0800

    add dict_type.py

---
+++ day2/dict_type.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author:pengbo
+info = {
+    'stu001':'zhangsan',
+    'stu002':'lisi',
+    'stu003':'wangwu',
+    'stu004':'alex'
+}
+info2 = {'stu001':'zhangsan','stu002':'lisi','stu003':'wangwu','stu004':'alex'}
+print (info)
