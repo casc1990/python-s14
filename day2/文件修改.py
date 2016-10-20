2ef0d6f02b167a927eec97884dacdb51c3a110c9
pengbo14 <pengbo@jd.com>
2016-10-20 23:45:55 +0800

    2016/10/20 23:29

---
+++ day2/文件修改.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author:pengbo
+f = open('yesterday','r',encoding='utf-8')
+f_new = open('yesterday_new','w',encoding='utf-8')
+for line in f:
+    #print (line.strip())
+    if '肆意的快乐等我享受' in line:
+        line = line.replace('肆意的快乐等我享受','肆意的快乐等Alex享受')
+    f_new.write(line)
+f.close()
+f_new.close()
