717af757f108b66be20e256f48fb59553d8b0241
pengbo14 <pengbo@jd.com>
2016-11-16 23:54:41 +0800

    生成器

--- day4/generator.py
+++ day4/generator.py
@@ -25,6 +25,56 @@ print (my_list)
 #for i in my_generator:print (i)   #生成器是可迭代的
 s = my_generator.__next__()  #生成器的__next__方法,下次访问或者循环就会从这个位置往后读取数据了
 print (s)
+s2 = next(my_generator)    #通过next函数也可以迭代生成器对象
+print (s2)
+#斐波拉契数列:除第一个和第二个数外，任意一个数都可由前两个数相加得到
+def fib(max):   #普通函数
+    n,a,b = 0,0,1  #n代表取几个值  a代表第一个元素   b代表第二个元素
+    while n < max:  #n定义了取几个值
+        print (b)
+        a,b=b,a+b   #a,b=b,a+b相当于t=(b,a+b) 则 a = t[0] ，b= t[1]
+        n+=1
+print ('华丽的分界线')
+fib(6)
+#生成器格式的斐波拉契数列
+#generator和函数的执行流程不一样。函数是顺序执行，遇到return语句或者最后一行函数语句就返回。
+#  而变成generator的函数，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+def fib2(max):
+    n,a,b=0,0,1
+    while n < max:
+        yield b   #如果一个函数定义中包含yield关键字,就是生成器
+        a,b=b,a+b
+        n = n + 1
+    return 'done'  #定义返回值，使用__nex__方法和next()函数可以拿到返回值，如果使用for不能得到返回值
+e = fib2(3)
+print (e.__next__())  #使用__next__方法
+print (e)
+
+#yield 例子
+def old():   #依次打印1,3,5
+    print('step 1')
+    yield 1  #报存命令的执行位置
+    print('step 2')
+    yield(3)
+    print('step 3')
+    yield(5)
+o = old()
+print (o.__next__())
+print (o.__next__())
+print (o.__next__())
+#last =o.__next__()
+#print (last)   #第四次已经没有了，所有报错了
+
+#生成器函数：使用__nex__方法和next()函数可以拿到返回值，如果使用for不能得到返回值
+g = fib2(6)
+while True:  #生成器对象使用循环是拿不到返回值的，无效循环，捕获异常
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:  #是这种类型的报错
+        print('Generator return value:', e.value)   #打印返回值
+        break
+
 
 
 
