02f3d2fdc54ed5afeb04d2dd06468d05aae4f614
casc1990 <962549206@qq.com>
2017-05-10 23:21:15 +0800

    socket-ssh的客户端和服务端模型

--- day8/socket-ssh-server.py
+++ day8/socket-ssh-server.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 #Author:pengbo
 __date__ = '2017/5/7 21:15'
-import socket,os
+import socket,os,time
 server = socket.socket()
 server.bind(('localhost',6677))
 server.listen(5)
@@ -23,6 +23,8 @@ while True:
         #发送命令结果分两步，先发送命名结果的长度，在发结果；因为客户端每次接收命令大小是有限的
         conn.send(str(len(cmd_res.encode())).encode('utf-8')) #发送命令结果的长度给客户端（因为可能有中文，所有先转换为bytes类型
             #在统计长度（len(cmd_res.encode()），然后在将数字转换为字符串，在将字符串转换为bytes类型 ）
+        #time.sleep(0.5) #可以使用os.time(0.5)强制超时,因为可能会粘包
+        client_ack = conn.recv(1024) #等待客户端确认接收。 因为recv是阻塞的，所以就阻断了send的继续发送数据，达到了防止粘包的问题
         conn.send(cmd_res.encode('utf-8'))  #发送命令执行结果
         print ('send done!')
 
