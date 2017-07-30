54833c7347222fb575536c8e3a04037f852be988
casc1990 <962549206@qq.com>
2017-07-30 12:26:27 +0800

    pytrhon基础教程

--- python基础教程/条件-循环.py
+++ python基础教程/条件-循环.py
@@ -76,9 +76,73 @@ print (ord('a')) #输出97 （查询a的asll码）
 result = 2 if 1+1==3 else 3
 print (result) #输出3 （如果1+1==3成立，result=2，否则result=3）
 age = 1
-assert 0<age<100 #输出True
+assert 0<age<100  #输出True
 age =-1
-assert 0<age<100 #报错
-assert 0<age<100,'the age must be realistic'  #报错，报错里会打出指定的字符串。（解释断言）
+#assert 0<age<100 #报错
+#assert 0<age<100,'the age must be realistic'  #报错，报错里会打出指定的字符串。（解释断言）
 #断言就是判断一句话的是否为真
+
+
+#while循环（当while条件为真，一致循环）
+q = 1
+while q < 100:
+    print (q)
+    q += 1
+#假如q小于100，执行循环体里的内容
+
+name = ''
+while not name:
+    name = input('Please enter your name? ').strip()  #strip去除输入左右两边的空格
+print ('Hello！%s' % name)
+#使用while循环确保用户输入了用户名
+
+#for循环（在指定的范围内循环）
+words = ['this','is','an','ex','parrot']
+for i in words:
+    print (i)
+#使用for循环，可以循环可迭代对象
+
+#range（范围函数）
+range(5,10) #5-9
+range(20) #0-19
+range(0,20,2) #0-19之间的偶数（步进为2）
+#xrange和range使用方法一致，区别在于xrange是生成器对象（延时生成），range一次性创建整个序列。xrange适合大序列
+
+d = {'x':1,'y':2,'z':3}
+#字典循环方法一
+for i in d:
+    print ('%s value is %s' %(i,d[i]))
+#字典循环方法二
+for key,value in d.items():
+    print ('%s value is %s' %(key,value))
+
+#迭代工具
+names = ['anne','beth','george','damon']
+age = [12,45,32,102]
+for i in range(len(names)):
+    print (names[i],'is',age[i],'years old')
+#并行迭代案例（并行迭代对象的长度要相等）
+
+www = zip(names,age)
+print (www) #输出 [('anne',12),('beth',45),('george',32),('damon',102)]
+zip(range(5),range(8)) #[(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]   以长度最短的序列为准
+#zip(序列1，序列2,序列N..)变量解包,将给定的序列组成元祖的列表，以长度最短的序列为准
+
+for name,age in zip(names,age):
+    print (name,'is',age,'years old!')
+#用zip函数迭代序列
+
+names = ['anne','beth','george','damon']
+for index,key in enumerate(names):
+    print (key,'is index',int(index)-1)
+#enumerate(seq) 打印序列的编号和值
+
+names = ['anne','beth','george','damon']
+names.sort() #原地修改，无返回值（排序）
+names.reverse() #原地修改，无返回值（反转）
+print (sorted(names)) #排序，不修改原数据
+print (list(reversed(names))) #反转，不修改原数据
+#sort、reverse 修改原数据  sorted、reversed 不修改原数据
+
+
 
